cff5be3e0b0b8cdab01ff09d290e3cd6617c6fef
Nanxi <nanxi0707@gmail.com>
2022-11-17 17:43:31 -0500

    commit

--- tests/testthat/test-spectral.R
+++ tests/testthat/test-spectral.R
@@ -2,7 +2,7 @@ test_that(
   "The spec_sig() is correct for time-series data with columns X, Y, Z.",
   {
     data(ukb_accel)
-    p <- spec_sig(ukb_accel[1:100, ],take_log = TRUE)
+    p <- spec_sig(ukb_accel[1:100, ], take_log = TRUE)
     expect_true(inherits(p$X, "numeric"))
     expect_true(inherits(p$Y, "numeric"))
     expect_true(inherits(p$Z, "numeric"))
