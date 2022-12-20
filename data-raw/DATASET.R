397ac18cb216212d22f6baa1a33ac8fdab0268df
Nanxi <nanxi0707@gmail.com>
2022-12-20 18:40:54 -0500

    final project

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -2,3 +2,9 @@
 
 ukb_accel <- readRDS("accel.rds")
 usethis::use_data(ukb_accel, overwrite = TRUE)
+
+adsl <- readRDS("adsl.rds")
+usethis::use_data(adsl, overwrite = TRUE)
+
+biomark <- readRDS("biomark.rds")
+usethis::use_data(biomark, overwrite = TRUE)
